1605e6e1f04c9ab4aa3d08944783fd4c83e351f0
Mr Ahmed Ali <itsahmedali21@gmail.com>
2025-05-16 13:20:41 +0600

    Commit

--- script.js
+++ script.js
@@ -6,10 +6,3 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 function toggleMenu() {
     document.getElementById("leftMenu").classList.toggle("show");
 }
-
-const priceRange = document.getElementById('priceRange');
-const priceValue = document.getElementById('priceValue');
-
-priceRange.addEventListener('input', function () {
-    priceValue.textContent = priceRange.value;
-});
